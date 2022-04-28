b7d4e5b62d7af2fc16b679e41ad70d25a246a7fe
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-28 08:57:59 +0200

    añadido el pokemon correctamente con el csv previamente puesto

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import JMPEstadisticas as jmp
 if __name__ == '__main__':
     eleccion = int(input(("¿Qué quieres ejecutar? \n 1: Juego de Pokémon \n 2: Análisis de Datos\n")))
     if eleccion == 1:
-        
+        pass
     elif eleccion ==2:
         datos = pnd.read_csv("imdb_sup.csv", header=0 , sep =",")
         lista_notas = list(datos["Rating"])
