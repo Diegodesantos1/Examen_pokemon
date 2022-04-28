7c14252635482f86607b2e51c6a4b926268df6a2
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-28 09:16:26 +0200

    metido en una carpeta

--- analisis de datos/main.py
+++ analisis de datos/main.py
@@ -1,13 +1,8 @@
 import pandas as pnd
 import JMPEstadisticas as jmp
-import modules_old
 
 
 if __name__ == '__main__':
-    eleccion = int(input(("¿Qué quieres ejecutar? \n 1: Juego de Pokémon \n 2: Análisis de Datos\n")))
-    if eleccion == 1:
-        import modules_old
-    elif eleccion ==2:
         datos = pnd.read_csv("Pokemon.csv", header=0 , sep =",")
         lista_notas = list(datos["Type"])
         observaciones = pnd.DataFrame({'Type': lista_notas})
