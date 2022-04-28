61162fc5245d91334854da1c50a82a2e8ac6cceb
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-28 09:15:15 +0200

    funciona el main_old

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pandas as pnd
 import JMPEstadisticas as jmp
+import modules_old
 
 
 if __name__ == '__main__':
     eleccion = int(input(("¿Qué quieres ejecutar? \n 1: Juego de Pokémon \n 2: Análisis de Datos\n")))
     if eleccion == 1:
-        pass
+        import modules_old
     elif eleccion ==2:
         datos = pnd.read_csv("Pokemon.csv", header=0 , sep =",")
         lista_notas = list(datos["Type"])
