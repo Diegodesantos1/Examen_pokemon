4f0bcae4a74f93c3f4c82779877517c25bea7113
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2022-04-28 08:50:48 +0200

    añadido el csv, el archivo de analisis de datos, creado el main y subido el pokemon

---
+++ main.py
@@ -0,0 +1,15 @@
+import pandas as pnd
+import JMPEstadisticas as jmp
+
+
+if __name__ == '__main__':
+    eleccion = int(input(("¿Qué quieres ejecutar? \n 1: Juego de Pokémon \n 2: Análisis de Datos\n")))
+    if eleccion == 1:
+        
+    elif eleccion ==2:
+        datos = pnd.read_csv("imdb_sup.csv", header=0 , sep =",")
+        lista_notas = list(datos["Rating"])
+        observaciones = pnd.DataFrame({'NOTAS': lista_notas})
+        #--- ANALISIS DE UNA CARACTERISTICA ---
+        stats = jmp.JMPEstadisticas(observaciones['NOTAS'])
+        stats.analisisCaracteristica()
